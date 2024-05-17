5a60cf5336b0a61b474d50ffd8f6b2c33c510c39
Tim White <timwhite3003@gmail.com>
2024-05-18 00:40:30 +0100

    Downloaded URLs get saved to .txt file

--- main.py
+++ main.py
@@ -35,6 +35,38 @@ def prompt_for_download_path():
     return download_path
 
 if __name__ == "__main__":
+    # Check if saved_urls.txt exists
+    if not os.path.exists('saved_urls.txt'):
+        # Create the file if it doesn't exist
+        open('saved_urls.txt', 'w').close()
+
+    else:
+        # Ask the user if they want to download the videos in saved_urls.txt
+        with open('saved_urls.txt', 'r') as file:
+            urls = file.readlines()
+            if urls:
+                # print a count of the urls
+                print(f"\n  Found \033[92m{len(urls)}\033[0m saved URLs.  ")
+
+                download_saved = input("Do you want to download these videos? (y/n): \n")
+                if download_saved.lower() == 'y':
+                    download_path = prompt_for_download_path()
+
+                    if not download_path:
+                        print("Download location not selected. Exiting.")
+                    else:
+                        for i, url in enumerate(urls):
+                            success, audio_file, title = download_youtube_audio(url.strip(), download_path)
+
+                            if success:
+                                print(f"\n \033[92m{title}: downloaded successfully! \033[0m\n")
+                                print(f"Progress: \033[92m{((i + 1) / len(urls)) * 100:.2f}%\033[0m")
+                            else:
+                                print("\033[91m Failed to download the audio. \033[0m\n")
+
+                else:
+                    print("Exiting. Moving onto main program. \n")                
+
     download_path = prompt_for_download_path()
 
     if not download_path:
@@ -50,6 +82,10 @@ if __name__ == "__main__":
 
             if success:
                 print("\n \033[92m" + title + ": downloaded successfully! \033[0m\n")
+                # Open the file in append mode
+                with open('saved_urls.txt', 'a') as file:
+                    # Write each line followed by a newline character
+                    file.write(youtube_url + "\n")
 
                 # Optionally, you can delete the downloaded video file
                 # os.remove(video_file)
